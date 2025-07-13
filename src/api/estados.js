72790a1b8976632bdcb5b90dc0a12a525141cf77
RamonMartinez01 <mafrtinezcjr1@gmail.com>
2025-07-13 16:17:19 -0700

    feat: Crea SelectorEstado.jsx Para enviar selección al estado global, se renderizará dentro de HomePage. - Agrega endpoint /estados para utilizar en SelectorEstados

--- src/api/estados.js
+++ src/api/estados.js
@@ -1,15 +1,18 @@
 import { api } from "./index";
 
+// Encuentra todos los estados SIN municipios (SIN GEOM)
+export const getAllEstados = () => api.get("/estados");
+
 // Encuentra solo los municipios que tengan relación con el estado por CVE_ENT
 export const getMunicipiosPorClaveEntidad = (cve_ent) =>
     api.get(`/estados/${cve_ent}/municipios`);
   
-  // Encuentra UN municipio específico por CVE_ENT y CVE_MUN
-  export const getMunicipioPorClaveMunicipio = (cve_ent, cve_mun) =>
-    api.get(`/estados/${cve_ent}/municipios/${cve_mun}`);
-  
-  // Encuentra todas las localidades de un municipio de un estado por CVE_ENT y CVE_MUN
-  export const getLocalidadesPorMunicipioEstado = (cve_ent, cve_mun) =>
-    api.get(`/estados/${cve_ent}/municipios/${cve_mun}/localidades`);
+// Encuentra UN municipio específico por CVE_ENT y CVE_MUN
+export const getMunicipioPorClaveMunicipio = (cve_ent, cve_mun) =>
+  api.get(`/estados/${cve_ent}/municipios/${cve_mun}`);
+
+// Encuentra todas las localidades de un municipio de un estado por CVE_ENT y CVE_MUN
+export const getLocalidadesPorMunicipioEstado = (cve_ent, cve_mun) =>
+  api.get(`/estados/${cve_ent}/municipios/${cve_mun}/localidades`);
   
  
