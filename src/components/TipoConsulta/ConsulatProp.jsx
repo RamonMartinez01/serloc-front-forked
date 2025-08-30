b64c7c1b7b386da55fab62f95696b257999e7d41
RamonMartinez01 <mafrtinezcjr1@gmail.com>
2025-08-30 07:38:47 -0700

    Agrega esqueleto base para ConsulatProp

--- src/components/TipoConsulta/ConsulatProp.jsx
+++ src/components/TipoConsulta/ConsulatProp.jsx
@@ -1,10 +1,24 @@
 import React from "react"
+import SelectorEstado from "../SelectorEstado";
+import SelectorMunicipio from "../SelectorMunicipio";
 
 const ConsultaProp = () => {
 
     return (
-        <div>
-            <h1>Consulta Prop</h1>
+        <div className="consulta-prop">
+            <p>Consulta de Propiedad Social (en construcción)</p>
+            <SelectorEstado />
+            <SelectorMunicipio />
+            {/* Próximamente: Selector Ejido / Comunidad */}
+            <select disabled>
+                <option>Ejido o Comunidad (pendiente)</option>
+            </select>
+
+            <label>Año:</label>
+            <select disabled>
+                <option>2010</option>
+                <option>2020</option>
+            </select>
         </div>
     );
 };
